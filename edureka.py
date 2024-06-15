c35b13e1ae76a04228f2cad0d97bbd7caea4e4d6
YOUNESS <younesmahdoubi1@gmail.com>
2024-06-15 03:51:41 +0100

    First Commit

---
+++ edureka.py
@@ -0,0 +1,8 @@
+# Print a welcome message to the terminal
+print("Hello, World!")
+
+# Ask the user for their name
+name = input("What is your name? ")
+
+# Greet the user
+print(f"Nice to meet you, {name}!")
